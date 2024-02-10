d38bf29df140c06d983afc3933383d2518654b70
Ezekiel Sampson Okon <125266196+ezechrissam@users.noreply.github.com>
2024-02-10 08:23:22 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const chart = document.querySelector(".chart");
+
+const chartBar = document.createElement("div");
+chartBar.classList.add("chart__bar");
+
+function fetchData(url) {
+  const xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url, false);
+  xmlHttp.send(null);
+  return { data: JSON.parse(xmlHttp.responseText) };
+}
+
+response = fetchData("./data.json");
+
+console.log(response.data);
+
+// Thanks to @pooja201095
+const maxAmount = Math.max(...response.data.map((el) => el.amount));
+
+response.data.forEach((el, i) => {
+  const barHeight = (el.amount / maxAmount) * 100;
+  const positionTop = 100 - barHeight;
+  const highlight =
+    (new Date().getDay() + 6) % 7 == i ? "chart__bar--highlight" : "";
+
+  chart.innerHTML += `
+    <div class="chart__bar ${highlight}" style="height: ${barHeight}%; top: ${positionTop}%" >
+      <div class="tooltip">$${el.amount}</div>
+      <div class="chart__bar__date">${el.day}</div>
+    </div>
+  `;
+});
